62de64e14ad655553ce45830122bb694b9562110
yuhsin-huang <97138478+yuhsin-huang@users.noreply.github.com>
2024-04-13 02:24:37 -0400

    add quiz summary page

--- server.py
+++ server.py
@@ -91,6 +91,83 @@ coffees = [
     }
 ]
 
+quizzes = [
+    {
+        "id": 1,
+        "name": "Latte",
+        "image": "static/images/latte.png",
+        "info": "Espresso blended with silky steamed milk for a creamy texture and balanced flavor profile.",
+        "recipe": [("espresso", 2), 
+                   ("steamed milk", 3), 
+                   ("milk foam", 1)],
+        "input": [("espresso", 2), 
+                   ("steamed milk", 3), 
+                   ("milk foam", 1)],
+        "correct": True
+    },
+    {
+        "id": 2,
+        "name": "Flat White",
+        "image": "static/images/flat_white.png",
+        "learned": False,
+        "info": "A bold espresso shot with a smooth layer of microfoam, creating a velvety texture and strong coffee taste.",
+        "recipe": [("espresso", 1), 
+                   ("steamed milk", 2)],
+        "input": [("espresso", 1), 
+                   ("steamed milk", 2)],
+        "correct": True
+    },
+    {
+        "id": 3, 
+        "name": "Americano",
+        "image": "static/images/americano.png",
+        "learned": True,
+        "info": "Hot water added to espresso for a lighter coffee experience, maintaining the espresso's robust flavor.",
+        "recipe": [("espresso", 1), 
+                   ("water", 1)],
+        "input": [("espresso", 1), 
+                   ("water", 1)],
+        "correct": True
+    },
+    {
+        "id": 4, 
+        "name": "Mocha",
+        "image": "static/images/mocha.png",
+        "learned": False,
+        "info": "Rich espresso infused with decadent chocolate syrup and creamy steamed milk, offering a delightful blend of bitter and sweet notes.",
+        "recipe": [("espresso", 1),
+                   ("hot chocolate", 1),
+                   ("steamed milk", 2), 
+                   ("whipped cream", 1)],
+        "input": [("espresso", 1), 
+                   ("steamed milk", 2)],
+        "correct": False
+    },
+    {
+        "id": 5, 
+        "name": "Cappuccino",
+        "image": "static/images/cappuccino.png",
+        "learned": True,
+        "info": "A harmonious combination of espresso, steamed milk, and frothed milk, resulting in a creamy texture and bold coffee flavor.",
+        "recipe": [("espresso", 1), 
+                   ("steamed milk", 1), 
+                   ("milk foam", 1)],
+        "input": [("espresso", 1), 
+                   ("steamed milk", 2)],
+        "correct": False
+    },
+    {
+        "id": 6, 
+        "name": "Espresso",
+        "image": "static/images/espresso.png",
+        "learned": False,
+        "info": "A concentrated coffee shot brewed by forcing hot water through finely-ground coffee beans, producing an aromatic beverage with a layer of crema on top.",
+        "recipe": [("espresso", 3)],
+        "input": [("espresso", 3)],
+        "correct": True
+    }
+]
+
 @app.route('/')
 def home():
    return render_template('home.html')
@@ -99,5 +176,31 @@ def home():
 def exploration():
    return render_template("exploration.html", coffees=coffees)
 
+@app.route('/quiz/summary')
+def quiz_summary():
+    def count_correct(quizzes):
+        count = 0
+        for quiz in quizzes:
+            if quiz['correct']:
+                count+=1
+        return count
+
+    return render_template('quiz_summary.html', quizzes=quizzes, count=count_correct(quizzes))
+
+@app.route('/quiz/summary/<quiz_id>')
+def quiz_summary_detail(quiz_id):
+    def find_quiz_by_id(quiz_id):
+        for quiz in quizzes:
+            if quiz['id'] == int(quiz_id):
+                return quiz
+        return None 
+
+    quiz = find_quiz_by_id(quiz_id)
+    if quiz:
+        return render_template('quiz_summary_detail.html', quiz=quiz)
+    else:
+        return "Quiz not found", 404
+
+    
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/quiz_summary.js
@@ -0,0 +1,46 @@
+function display_quiz_summary() {
+  $("#all-quizzes").empty();
+
+  if (!Array.isArray(quizzes)) {
+    console.error("Quizzes data is not an array or is undefined");
+    return;
+  }
+
+  quizzes.forEach(function (q, index) {
+    let $card = $(`
+      <div class='quizsummarycard col-md-3' data-id=${q["id"]}>
+          <div class="row">
+            <div class="col-7">
+                <h2>Quiz ${q["id"]}</h2>
+            </div>
+            <div class="col-5">
+              <img src='/static/images/${
+                q["correct"] ? "check" : "unchecked"
+              }.png' alt='correct'>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <p>Could you make me a cup of ${q["name"]}?</p>
+            </div>
+          </div>
+      </div>
+    `);
+
+    $("#all-quizzes").append($card);
+
+    $card.on(
+      "click",
+      (function (quizId) {
+        return function () {
+          window.location.href = `/quiz/summary/${quizId}`;
+        };
+      })(q["id"])
+    );
+  });
+}
+
+$(document).ready(function () {
+  document.getElementById("correctCount").textContent = count;
+  display_quiz_summary();
+});
